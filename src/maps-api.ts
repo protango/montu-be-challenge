76d1bc26b260425af64d6b6540cc0c0f34dbe8e5
Zachary Murray <zacharymurray@hotmail.com>
2024-08-14 16:13:31 +1000

    Add country limit option to getPlaceAutocomplete

--- src/maps-api.ts
+++ src/maps-api.ts
@@ -1,17 +1,29 @@
 import axios from 'axios'
 import { FuzzySearchResults } from '../types/maps-api';
+import { CountryCode } from '../types/countryCode';
 
-// https://developer.tomtom.com/search-api/documentation/search-service/fuzzy-search
-export async function getPlaceAutocomplete(key: string, address: string) {
+/**
+ * Call the fuzzy search API to complete an incomplete place or address
+ * @param key The TomTom API Key
+ * @param address The incomplete address to search for
+ * @param options Options for the search
+ * @returns A list of places which match the search address
+ * @link https://developer.tomtom.com/search-api/documentation/search-service/fuzzy-search
+ */
+export async function getPlaceAutocomplete(key: string, address: string, options?: {
+    /** Limit the search to countries in this array */
+    countrySet?: CountryCode[],
+    /** Maximum number of search results, default 100 */
+    limit?: number
+}): Promise<FuzzySearchResults['results']> {
     const autocomplete = await axios.get<FuzzySearchResults>(`https://api.tomtom.com/search/2/search/${address}.json'`, {
         params: {
             key,
-            limit: 100,
+            limit: options?.limit ?? 100,
+            countrySet: options?.countrySet?.join(',')
         }
     });
-    return autocomplete.data.results.map((result) => {
-        return {
-            placeId: result.id,
-        }
-    })
+
+    // Return complete results
+    return autocomplete.data.results;
 }
