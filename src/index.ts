cc9761caa84af52e73cfdefda75a695c832de9de
Zachary Murray <zacharymurray@hotmail.com>
2024-08-14 16:25:46 +1000

    Add static type for getAutoCompleteDetails return value

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import { AutocompleteDetails } from '../types';
 import { CountryCode } from '../types/countryCode';
 import { CONFIG } from './config';
 import { getPlaceAutocomplete } from './maps-api'
 
-export async function getAutoCompleteDetails(address: string) {
+export async function getAutoCompleteDetails(address: string): Promise<AutocompleteDetails[]> {
     const apiKey = CONFIG.TOMTOM_API_KEY;
     // get autocomplete results
     const rawResults = await getPlaceAutocomplete(apiKey, address, {
@@ -11,10 +12,10 @@ export async function getAutoCompleteDetails(address: string) {
     });
 
     // map results to required type
-    return rawResults.map(result => ({
+    return rawResults.map<AutocompleteDetails>(result => ({
         placeId: result.id,
         streetNumber: result.address.streetNumber,
-        countryCode: result.address.countryCode,
+        countryCode: result.address.countryCode as CountryCode,
         country: result.address.country,
         freeformAddress: result.address.freeformAddress,
         municipality: result.address.municipality
