e87d62081bc0d16cbc980491f67e5cf83cf8065f
Zachary Murray <zacharymurray@hotmail.com>
2024-08-14 15:06:08 +1000

    Add API return types

--- src/maps-api.ts
+++ src/maps-api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
+import { FuzzySearchResults } from '../types/maps-api';
 
 // https://developer.tomtom.com/search-api/documentation/search-service/fuzzy-search
 export async function getPlaceAutocomplete(key: string, address: string) {
-    const autocomplete = await axios.get(`https://api.tomtom.com/search/2/search/${address}.json'`, {
+    const autocomplete = await axios.get<FuzzySearchResults>(`https://api.tomtom.com/search/2/search/${address}.json'`, {
         params: {
             key,
             limit: 100,
